243028a70f29919734068c6c2d1e31939eec65c1
Scott Esbrandt <scott.esbrandt@snyk.io>
2022-03-17 00:11:03 -0400

    change requires to imports

--- issues-to-github-issues/index.mjs
+++ issues-to-github-issues/index.mjs
@@ -1,5 +1,7 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+//const core = require("@actions/core");
+//const github = require("@actions/github");
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as fs from 'fs';
 import * as path from 'path';
 import * as debugLib from 'debug';
